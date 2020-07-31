7de610f61cfc14f7121d94e844e08d4ed72b4519
Rachel Choe <rachelchoe@Rachels-MacBook-Pro.local>
2020-07-31 16:35:18 -0700

    ta

--- ArrayString.cpp
+++ ArrayString.cpp
@@ -1,9 +1,23 @@
 #include <string>
 #include <iostream>
+#include <unordered_map>
+
 using namespace std;
 
 int main(){
-  cout << "Hello world" << endl;
+  cout << "Printing solutions of questions" << endl;
 
+  isunique("cata");
   return 0;
+}
+
+//determine if the string has unique characters
+bool isUnique(string s){
+    //unordered_map<string, int> stringkey;
+
+    for (char& c : s){
+      cout << c << endl;
+    }
+    return true;
+
 }
